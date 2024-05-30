058d1f7a0b34a39e714e1a10c37712893de02d5c
Timofey Vishnepolskiy <timofey@reply.io>
2024-05-30 12:55:01 +0300

    base event handlers logic implemented

---
+++ Infrastructure/Events/BackgroundEventHandlerRunner.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Events;
+
+public interface IBackgroundEventHandlerRunner
+{
+    void RunEventHandler(Type eventHandlerType, EventArgs args);
+}
+
+public class BackgroundEventHandlerRunner : IBackgroundEventHandlerRunner
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+    public BackgroundEventHandlerRunner(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public void RunEventHandler(Type eventHandlerType, EventArgs args)
+    {
+        //todo: replace with queue
+        Task.Factory.StartNew(async () =>
+        {
+            using var scope = _serviceScopeFactory.CreateScope();
+
+            try
+            {
+                var handler = (IEventHandler) scope.ServiceProvider.GetRequiredService(eventHandlerType);
+                await handler.Invoke(args, _cancellationTokenSource.Token);
+            }
+            catch (Exception e)
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
+                logger.LogError(e.InnerException ?? e, $"{nameof(BackgroundEventHandlerRunner)} Failed to run task");
+            }
+        });
+    }
+}
---
+++ Infrastructure/Handlers/DI/HandlersDependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Events;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Handlers.DI;
+
+public static class HandlersDependencyInjection
+{
+    public static IServiceCollection AddHandlers(this IServiceCollection services)
+    {
+        services.Scan(x => x
+                .FromAssemblies(Assembly.GetExecutingAssembly())
+                .AddClasses(c => c.AssignableTo<IEventHandler>())
+                .AsMatchingInterface());
+        
+        return services;
+    }
+}

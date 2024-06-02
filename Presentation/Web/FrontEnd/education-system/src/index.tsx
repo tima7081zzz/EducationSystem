387767dd4a4740dbd140d9056747135c3a1e4db8
Timofey Vishnepolskiy <timofey@reply.io>
2024-06-02 13:08:29 +0300

    FE toast using in index.tsx

--- Presentation/Web/FrontEnd/education-system/src/index.tsx
+++ Presentation/Web/FrontEnd/education-system/src/index.tsx
@@ -4,14 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/HomePage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
+  <ToastContainer
+    position="top-center"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    />
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
